fc941c977fa98d1f66552230dc5368589c08d773
Junyoung Lee <leejy31415@outlook.com>
2025-12-01 02:30:52 -0600

    Day 1 - Part 1

---
+++ 1/a.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int mod(int a, int m) noexcept
+{
+    return (a % m + m) % m;
+}
+
+int main()
+{
+    int dial = 50;
+    int cnt = 0;
+    
+    std::string s;
+    while (std::cin >> s)
+    {
+        const int d = std::atoi(&s[1]);
+        dial += (s[0] == 'R') ? d : -d;
+        dial = mod(dial, 100);
+
+        if (dial == 0)
+        {
+            cnt++;
+        }
+    }
+
+    std::cout << cnt << '\n';
+}

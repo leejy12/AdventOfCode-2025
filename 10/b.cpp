95f76c9a582cebe331c4ca0bb5d7ee6938010e85
Junyoung Lee <leejy31415@outlook.com>
2026-01-20 04:55:30 -0600

    day 10 part 2 wip

---
+++ 10/b.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <print>
+#include <ranges>
+#include <string>
+#include <string_view>
+#include <vector>
+
+void gauss_elim(std::vector<std::vector<int>>& m)
+{
+    int lead = 0;
+    const int numRows = static_cast<int>(m.size());
+    const int numCols = static_cast<int>(m[0].size());
+
+    for (int r = 0; r < numRows; r++)
+    {
+        if (numCols <= lead)
+        {
+            break;
+        }
+
+        int i = r;
+        while (m[i][lead] == 0)
+        {
+            i++;
+            if (numRows == i)
+            {
+                i = r;
+                lead++;
+                if (numCols == lead)
+                {
+                    break;
+                }
+            }
+        }
+
+        std::swap(m[i], m[r]);
+
+        if (m[r][lead] != 0)
+        {
+            int divisor = m[r][lead];
+            for (int& v : m[r])
+            {
+                v /= divisor;
+            }
+        }
+
+        for (int j = 0; j < numRows; j++)
+        {
+            if (j != r)
+            {
+                int factor = m[j][lead];
+                for (int k = 0; k < numCols; k++)
+                {
+                    m[j][k] -= factor * m[r][k];
+                }
+            }
+        }
+
+        lead++;
+    }
+}
+
+int main()
+{
+    std::string s;
+    // int answer = 0;
+
+    while (std::getline(std::cin, s))
+    {
+        const size_t light_end = s.find(']');
+        const int num_lights = static_cast<int>(light_end - 1);
+        const size_t switch_start = s.find('(');
+        const size_t jolt_start = s.find('{');
+
+        std::vector<std::vector<int>> m;
+        m.resize(num_lights);
+
+        for (size_t i = switch_start; i < jolt_start; i++)
+        {
+            if (s[i] == '(')
+            {
+                // new column
+                for (auto& row : m)
+                    row.push_back(0);
+            }
+            else if (s[i] >= '0' && s[i] <= '9')
+            {
+                m[s[i] - '0'].back() = 1;
+            }
+        }
+
+        std::string_view jolt_sv(s.c_str() + jolt_start + 1, s.size() - jolt_start - 2);
+        for (auto [row, rng] : std::views::enumerate(jolt_sv | std::views::split(',')))
+        {
+            std::string_view sv(rng);
+            int x{};
+            std::from_chars(sv.data(), sv.data() + sv.size(), x);
+            m[row].push_back(x);
+        }
+
+        gauss_elim(m);
+        std::println("{}", m);
+    }
+
+    // std::println("{}", answer);
+
+    // std::vector<std::vector<int>> v{
+    //     {0, 0, 0, 0, 1, 1, 3},
+    //     {0, 1, 0, 0, 0, 1, 5},
+    //     {0, 0, 1, 1, 1, 0, 4},
+    //     {1, 1, 0, 1, 0, 0, 7},
+    // };
+
+    // std::vector<std::vector<int>> v{
+    //     {1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 72},
+    //     {0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 88},
+    //     {0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 59},
+    //     {0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 60},
+    //     {1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 69},
+    //     {0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 36},
+    //     {0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 53},
+    //     {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 64},
+    //     {0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 58},
+    //     {0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 81},
+    // };
+
+    // std::vector<std::vector<int>> v{
+    //     {1, 2, -1, -4},
+    //     {2, 3, -1, -11},
+    //     {-2, 0, -3, 22},
+    // };
+
+    // gauss_elim(v);
+
+    // for (const auto& row : v)
+    //     std::println("{}", row);
+}
